6bc8ad30b8872aa33481f1e696dc13cdeeb06b60
nakagawa331 <Nakagawasousei333>
2022-11-27 12:27:32 +0900

    initial commit

---
+++ src/main/java/com/asslutRacing/batch/AsslutRacingBatchApplication.java
@@ -0,0 +1,13 @@
+package com.asslutRacing.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsslutRacingBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsslutRacingBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asslutRacing/batch/AsslutRacingBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asslutRacing.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsslutRacingBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
